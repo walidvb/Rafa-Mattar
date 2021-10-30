cb56003c48d71a570c4ff9a15aab8a95a83d4f1e
Walid <hello@walidvb.com>
2021-10-30 19:15:23 +0200

    fix build

--- shared/layout/Layout.tsx
+++ shared/layout/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = ({ children, className, fullLogo = false }: { children: ReactNode, className?: string, fullLogo: boolean}) => {
+const Layout = ({ children, className, fullLogo = false }: { children: ReactNode, className?: string, fullLogo?: boolean}) => {
 
   return <div className="min-h-screen flex flex-col font-serif">
     <Header fullLogo={fullLogo}/>
