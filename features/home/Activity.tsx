226dcb44e97f74832a933490bb2826d80151bcc6
Walid <hello@walidvb.com>
2021-12-22 17:58:18 -0300

    adds header height

--- features/home/Activity.tsx
+++ features/home/Activity.tsx
@@ -3,28 +3,35 @@ import Image from 'next/image';
 
 export const Activity = () => {
   const [images, setImages] = useState([]);
-
+  const [err, setErr] = useState(false);
+  
   useEffect(() => {
     const url = `https://graph.instagram.com/me/media?fields=media_count,media_type,permalink,media_url,caption&&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_API_TOKEN}`;
     (async () => {
-      const response = await fetch(url);
-      const { data } = await response.json();
-      console.log(data);
-      setImages(data);
+      try{
+        const response = await fetch(url);
+        const { data } = await response.json();
+        console.log(data);
+        setImages(data);
+      } catch(err){
+        console.log(err);
+      }
     })();
   }, [setImages]);
 
-  return <div id="actualite" className="mx-auto container  mt-12 md:mt-32 px-2 md:px-0">
+  return <div id="actualite" className="mx-auto container  pt-12 md:mt-32 px-2 md:px-0" style={{ scrollMarginTop: "var(--header-height)" }}>
     <h2 className="text-3xl md:text-6xl font-bold mb-8 md:mb-16">Actualité</h2>
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-      {images.map((feed: IGFeed) => (
-        <a className="hover:opacity-75" href={feed.permalink} target="_blank" rel="noreferrer" key={feed.id} title={feed.caption}>
-          {(feed.media_type === 'IMAGE' || feed.media_type === 'CAROUSEL_ALBUM')
-            ? <Image alt={feed.caption} width={200} height={200} src={feed.media_url} key={feed.id} layout="responsive" />
-            : <video key={feed.id} src={feed.media_url} />}
-        </a>
-      ))}
-    </div>
+    {err ? <p>Une erreur est survenue...</p> : (
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {images.map((feed: IGFeed) => (
+          <a className="hover:opacity-75" href={feed.permalink} target="_blank" rel="noreferrer" key={feed.id} title={feed.caption}>
+            {(feed.media_type === 'IMAGE' || feed.media_type === 'CAROUSEL_ALBUM')
+              ? <Image alt={feed.caption} width={200} height={200} src={feed.media_url} key={feed.id} layout="responsive" />
+              : <video key={feed.id} src={feed.media_url} />}
+          </a>
+        ))}
+      </div>
+    )}
   </div>;
 };
 type IGFeed = {
--- features/home/ProjectList.tsx
+++ features/home/ProjectList.tsx
@@ -5,7 +5,7 @@ import { WithPointer } from './WithPointer';
 
 
 const ProjectList = ({ projects }: { projects: IProject[]}) => {
-  return <div id="projets">
+  return <div id="projets" style={{ scrollMarginTop: "var(--header-height)"}}>
     {projects.map((project) => <WithPointer pointerTitle="Voir le projet" key={project.sys.id}>
       <ProjectTeaser
         image={project.fields.homeImage}
