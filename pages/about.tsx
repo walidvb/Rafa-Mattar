e3ecef36595b072ea1fe178ceae5150b5eccab4d
Walid <hello@walidvb.com>
2021-12-23 21:42:12 -0300

    ahout mobile

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,13 +5,14 @@ import { ISiteSettings } from '../@types/contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
 import styled from 'styled-components'
+import React from 'react';
 
 const TeamMember = ({ img, name, className }: { img: string, name: string, className?: string }) => {
-  return <div className={`flex items-center justify-center gap-4 ${className}`}>
+  return <div className={`flex text-bold items-center justify-center gap-4 ${className}`}>
     <div className="rounded-full">
       <Image src={"https:" + img} alt={name} className="w-16 h-16 rounded-full" width={100} height={100}/>
     </div>
-    <div className="text-center">{name}</div>
+    <div>{name.split(' ').map(s => <React.Fragment key={s}>{s}<br/></React.Fragment>)}</div>
   </div>
 }
 
@@ -21,19 +22,19 @@ const Bleeding = styled.div`
 `
 
 const TeamPage = ({ settings }: { settings: ISiteSettings}) => {
-  return <Layout className="items-center flex justify-center mt-20">
+  return <Layout className="items-center flex justify-center md:mt-20">
     <Bleeding>
-      <div className="container mx-auto grid grid-cols-12 gap-4">
-        <div className="col-span-5">
+      <div className="container mx-auto md:grid grid-cols-12 gap-4 ">
+        <div className="col-span-5 max-h-[35vh] md:max-h-[initial] overflow-hidden">
           <Image src={"https:" + settings.fields.biographyImage.fields.file.url} layout="responsive" width={600} height={600} alt="Biography"/>
         </div>
-        <div className="grid md:grid-cols-5 bg-brand text-white col-span-7 py-8">
+        <div className="grid md:grid-cols-5 bg-brand px-2 md:px-0 gap-y-12 text-white col-span-7 py-8">
           <div className="col-span-5 col-start-2">
             {documentToReactComponents(settings.fields.about)}
           </div>
           <div className="flex justify-between col-span-5 col-start-2 items-end">
-            <TeamMember img={settings.fields.valeriaImage.fields.file.url} name={"Valeria Harari"} />
-            <TeamMember img={settings.fields.antoineImage.fields.file.url} name={"Antoine Harari"} className="flex-row-reverse" />
+            <TeamMember img={settings.fields.valeriaImage.fields.file.url} name={"Valeria Mazzucchi"} />
+            <TeamMember img={settings.fields.antoineImage.fields.file.url} name={"Antoine Harari"} className="flex-row-reverse text-right" />
           </div>
         </div>
       </div>
