7cffd43c5ac732d939e9cf5f7c4ad55aac87bc55
Walid <hello@walidvb.com>
2021-12-23 21:45:15 -0300

    max width instagram

--- features/home/Activity.tsx
+++ features/home/Activity.tsx
@@ -22,7 +22,7 @@ export const Activity = () => {
   return <div id="actualite" className="mx-auto container  pt-12 md:mt-32 px-2 md:px-0" style={{ scrollMarginTop: "var(--header-height)" }}>
     <h2 className="text-3xl md:text-6xl font-bold mb-8 md:mb-16">Actualité</h2>
     {err ? <p>Une erreur est survenue...</p> : (
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-3 gap-2 md:gap-8 max-w-[1000px] mx-auto">
         {images.map((feed: IGFeed) => (
           <a className="hover:opacity-75" href={feed.permalink} target="_blank" rel="noreferrer" key={feed.id} title={feed.caption}>
             {(feed.media_type === 'IMAGE' || feed.media_type === 'CAROUSEL_ALBUM')
