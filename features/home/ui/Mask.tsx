fa7e121cda3e18074b25011eef42d4bd442bd5f5
Walid <hello@walidvb.com>
2022-01-03 10:59:14 -0300

    with shader

--- features/home/ui/Mask.tsx
+++ features/home/ui/Mask.tsx
@@ -1,88 +1,79 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Image from 'next/image';
-import { map } from '../../../shared/helpers/map';
-import { BWImage } from "./BWImage";
+import React from "react";
+// @ts-ignore
+import { Shaders, Node, GLSL } from "gl-react";
+// @ts-ignore
+import { Surface } from "gl-react-dom";
+// @ts-ignore
+import { Motion, spring } from "react-motion";
 
+const shaders = Shaders.create({
+  helloBlue: {
+    frag: GLSL`
+  precision highp float;
+  varying vec2 uv;
+  uniform vec2 resolution;
+  uniform float scale;
+  const float STEP_SIZE = 0.02;
+  uniform sampler2D t;
 
-const ClippedImage = styled(Image)`
-  clip-path: url(#clip);
-`
-function MaskedImage({ className, ...props }: any) {
-  const [isHovered, setIsHovered] = useState(false);
-  
-  return (
-    // 3px padding as the image underneath moves slightly slower
-    // so you can see the borders
-    <Wrapper
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className={`relative flex justify-center p-[3px] ${className}`}
-    >
-      <ClippedImage {...props} objectFit="cover" />
-      <SVGMask isHovered={isHovered} />
-    </Wrapper>
-  );
-}
-
-export default MaskedImage;
-
-
-const STEP_SIZE = 0.04;
-const RESOLUTION = 1;
-
-const SVGMask = React.memo(function SVG({ isHovered } : { isHovered: boolean }) {
-  const circles = [];
-  const w = RESOLUTION;
-  const h = RESOLUTION;
-  const scale = isHovered ? 3 : 1
-  for (let x = 0; x < w; x += STEP_SIZE) {
-    for (let y = 0; y < h; y += STEP_SIZE) {
-      const distToCenter = Math.sqrt(
-        (x - w / 2) * (x - w / 2) + (y - h / 2) * (y - h / 2)
-      );
-
-      let r = map(distToCenter, w / 2, w / 6, 0, (STEP_SIZE / 2) * scale);
-      r = Math.min(r, (STEP_SIZE / 2) * scale);
-      if (r <= 0.0001) {
-        continue
-      }
-      circles.push(
-        <circle key={`${x}-${y}`} cx={x} cy={y} r={r} fill="white" />
-      );
-    }
+  float map(float num, float in_min, float in_max, float out_min, float out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
 
-  return (
-    <Svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${w} ${h}`} width="0" height="0">
-        <defs>
-          <clipPath clipPathUnits="objectBoundingBox"  id="clip">
-            {circles}
-          </clipPath>
-        </defs>
-    </Svg>
-  );
+  void main() {
+    vec2 loop = mod(uv, vec2(STEP_SIZE)) - STEP_SIZE/2.;
+    loop /= STEP_SIZE;
+    float distToCenter = sqrt((uv.x - 0.5) * (uv.x - 0.5) + (uv.y - 0.5) * (uv.y - 0.5));
+    float circle_size = map(distToCenter, 0.5, 0.0, 0.0, 0.70710678118);
+    circle_size = min(0.5, circle_size);
+    circle_size *= scale;
+    
+    bool is_in_circle = bool(length(loop) - circle_size <= 0.);
+
+
+    gl_FragColor = mix(
+      texture2D(t, uv),
+      vec4(0.0),
+      is_in_circle ? 0.0 : 1.0
+    );
+  }
+  `
+  }
 });
 
-const Svg = styled.svg`
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  mix-blend-mode: revert;
-  max-width: 100%;
-  max-height: 100%;
-  background: black;
-`
 
-const Wrapper = styled(BWImage)`
-  svg circle{
-    transition: all .8s .3s ease-in-out;
-  }
-  &:hover{
-    svg circle{
-      transition: all .5s ease-in-out;
-    }
-  }
-`
+const Mask = ({
+  src,
+  width,
+  height
+}: {
+  src: string;
+  width: number;
+  height: number;
+}) => {
+  const [scale, setScale] = React.useState(1.0);
+  return (
+    <div onMouseEnter={() => setScale(3.0)} onMouseLeave={() => setScale(1.0)}>
+      <Surface width={width} height={height}>
+        <Motion
+          defaultStyle={{ scale }}
+          style={{
+            scale: spring(scale, [5, 1])
+          }}
+        >
+          {({ scale }: { scale: number }) => (
+            <Node
+              shader={shaders.helloBlue}
+              uniforms={{
+                t: src,
+                scale
+              }}
+            />
+          )}
+        </Motion>
+      </Surface>
+    </div>
+  );
+};
+
+export default Mask;
