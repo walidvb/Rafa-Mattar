85ba7bc4c99eccc33c84fd3ed510a4a6baa671ed
Walid <hello@walidvb.com>
2022-01-05 16:04:27 -0300

    fixup! fixup! fixup! fixup! adds og

--- shared/layout/OGTags.tsx
+++ shared/layout/OGTags.tsx
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 const host = process.env.NEXT_PUBLIC_VERCEL_URL
 
-export default function OGTags({ title = 'Futur Proche', description = "Une société de!", image = '/images/og.jpg', path = '/' }: { title?: string, description?: string, image?: string, path?: string }) {
+export default function OGTags({ title = 'Futur Proche', description = "Une société de!", image = `${host}/images/og.jpg`, path = '/' }: { title?: string, description?: string, image?: string, path?: string }) {
   return <Head>
     <title>{title}</title>
     <meta property='og:url' content={`https://${host}/${path}`} />
     <meta property='og:type' content={"article"} />
     <meta property='og:title' content={title} />
     <meta property='og:description' content={description} />
-    <meta property='og:image' content={`https://${url}${image}`} />
+    <meta property='og:image' content={image} />
   </Head>
 }
