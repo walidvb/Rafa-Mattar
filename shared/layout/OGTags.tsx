0e8f804548efb90fbd3c959c376b768e9e0b065d
Walid <hello@walidvb.com>
2022-02-28 12:22:41 -0300

    tag

--- shared/layout/OGTags.tsx
+++ shared/layout/OGTags.tsx
@@ -2,13 +2,25 @@ import Head from 'next/head';
 
 const host = process.env.NEXT_PUBLIC_VERCEL_URL
 
-export default function OGTags({ title = 'Futur Proche', description = "Une société de!", image = `https://${host}/images/og.jpg`, path = '/' }: { title?: string, description?: string, image?: string, path?: string }) {
-  return <Head>
-    <title>{title}</title>
-    <meta property='og:url' content={`https://${host}/${path}`} />
-    <meta property='og:type' content={"article"} />
-    <meta property='og:title' content={title} />
-    <meta property='og:description' content={description} />
-    <meta property='og:image' content={image} />
-  </Head>
+export default function OGTags({
+  title = 'Futur Proche',
+  description = 'Une société de production',
+  image = `https://${host}/images/og.jpg`,
+  path = '/',
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  path?: string;
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:url" content={`https://${host}/${path}`} />
+      <meta property="og:type" content={'article'} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+    </Head>
+  );
 }
