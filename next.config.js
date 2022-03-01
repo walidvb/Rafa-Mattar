bb28433021a6fd4bc82964c33f775ac922acc42b
Walid <hello@walidvb.com>
2022-03-01 14:03:06 -0300

    adds redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['images.ctfassets.net', 'scontent.cdninstagram.com']
-  }
-}
+    domains: ['images.ctfassets.net', 'scontent.cdninstagram.com'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/lost-cells',
+        destination: '/documentaires/lost-cells',
+        permanent: true,
+      },
+      {
+        source: '/letincelle',
+        destination: '/documentaires/letincelle',
+        permanent: true,
+      },
+      {
+        source: '/grotesque',
+        destination: '/documentaires/grotesque',
+        permanent: true,
+      },
+    ];
+  },
+};
