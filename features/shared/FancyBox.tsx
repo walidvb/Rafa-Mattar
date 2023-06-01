9af86983e2984a4231ee4d8aa20d5e35fa6983c1
Walid <hello@walidvb.com>
2023-06-01 20:37:33 -0300

    fancy box stuff

--- features/shared/FancyBox.tsx
+++ features/shared/FancyBox.tsx
@@ -19,7 +19,30 @@ function Fancybox(props) {
     };
   });
 
-  return <div ref={containerRef}>{props.children}</div>;
+  return (
+    <div ref={containerRef}>
+      <div
+        className="z-[1051] fixed top-4 right-6 cursor-pointer closer"
+        onClick={() => NativeFancybox.close()}
+      >
+        <style jsx>
+          {`
+            :global(.with-fancybox) .closer {
+              opacity: 1;
+              pointer-events: auto;
+            }
+            .closer {
+              opacity: 0;
+              transition: opacity 0.1s ease-in-out;
+              pointer-events: none;
+            }
+          `}
+        </style>
+        fechar
+      </div>
+      {props.children}
+    </div>
+  );
 }
 
 export default Fancybox;
