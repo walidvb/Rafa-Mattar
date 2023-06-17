3081a31b30a3a2c51104ce4c062987c2588e60f1
Walid <hello@walidvb.com>
2023-06-16 21:06:38 -0300

    bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,27 @@
-export { default, getServerSideProps } from "./[slug]"
+// components/BackgroundImage.js
+import Link from 'next/link';
+import React from 'react';
+
+const BackgroundImage = () => {
+  const imageUrl = {
+    desktop: '/path/to/desktop/image.jpg',
+    mobile: '/path/to/mobile/image.jpg',
+  };
+
+  return (
+    <Link href="/filmes">
+      <div
+        className="bg-cover bg-center h-screen relative"
+        style={{
+          backgroundImage: `url(/images/bg.jpeg)`,
+        }}
+      >
+        <div className="absolute top-8 right-8">
+          <span className="text-xl text-white">Entrar</span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BackgroundImage;
