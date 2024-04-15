4af8c8493feb6f7ed56c5ecad4243caea36dd5e2
Walid <hello@walidvb.com>
2024-04-15 08:35:02 -0300

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" type="image/png" href="/favicon.png"></link>
+          <link rel="icon" type="image/png" href="/favicon.webp"></link>
           {/* @ts-ignore */}
           {this.props.styleTags}
         </Head>
